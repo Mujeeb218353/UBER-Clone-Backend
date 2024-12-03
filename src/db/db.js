2693956cb3b36660536e0d07cb6b0d4fff6332f8
Mujeeb Ur Rehman <mujeeb218353@gmail.com>
2024-12-04 00:54:12 +0500

    Edited db.js

--- src/db/db.js
+++ src/db/db.js
@@ -3,6 +3,10 @@ import { DB_NAME } from "../constants.js";
 
 
 const connectDB = async () => {
+    if (mongoose.connection.readyState === 1) {
+        console.log('Already connected to MongoDB');
+        return;
+    }
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
         console.log(`MongoDB Connected !!!\nDB NAME: ${DB_NAME}\nHOST: ${connectionInstance.connection.host}`);
