9f905601f2ef5fc895c1c4828c0f402c9556ac77
Mujeeb Ur Rehman <mujeeb218353@gmail.com>
2024-12-04 00:44:08 +0500

    Changes DB file name from index.js to db.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 import app from "./app.js";
 import http from "http";
-import { connectDB } from "./db/index.js";
+import { connectDB } from "./db/db.js";
 dotenv.config({
   path: "./.env",
 });
