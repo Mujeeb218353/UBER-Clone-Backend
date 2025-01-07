a76afde8aceffb362b8e29b8c7d38347d7df7a95
Mujeeb Ur Rehman <mujeeb218353@gmail.com>
2025-01-07 12:00:00 +0500

    Fixed a bug

--- src/routes/captain.routes.js
+++ src/routes/captain.routes.js
@@ -22,7 +22,7 @@ router.route("/register").post(
         body("vehicle.color").isLength({ min: 3 }).withMessage("Color must be at least 3 characters long"),
         body("vehicle.plate").isLength({ min: 3 }).withMessage("Plate must be at least 3 characters long"),
         body("vehicle.capacity").isLength({ min: 1 }).withMessage("Capacity must be at least 1"),
-        body("vehicle.vehicleType").isIn([ "car", "motorcycle", "auto" ]).withMessage("Invalid vehicle"),
+        body("vehicle.vehicleType").isIn([ "car", "motorcycle", "rikshaw" ]).withMessage("Invalid vehicle"),
     ],
     registerCaptain
 );
